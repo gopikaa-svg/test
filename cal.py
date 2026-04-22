cc3d84e18d8e5cefa65ee0f6fcee9f7fc4baa4aa
gopikaa-svg <g2911355@gmail.com>
2026-04-22 14:35:05 +0530

    test commit

---
+++ cal.py
@@ -0,0 +1,15 @@
+choice=input("enter your choice:")
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+print("selct operation")
+print("1.addition")
+print("2.subtraction")
+print("3.multiplication")
+if choice=='1':
+    print(num1+num2)
+elif choice=='2':
+    print(num1-num2)
+else:
+    print(num1*num2)
+
+   
